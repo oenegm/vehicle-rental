f3b0ca095da7c144050ea88d221d2fed72f47ffc
oenegm <oenegm@gmail.com>
2022-10-19 14:19:07 +0200

    Add docker-compose.yml.
    Add Vehicle Entity.
    
    Signed-off-by: oenegm <oenegm@gmail.com>

---
+++ src/main/java/com/project/vehiclerental/model/Maker.java
@@ -0,0 +1,21 @@
+package com.project.vehiclerental.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "maker")
+public class Maker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+}
