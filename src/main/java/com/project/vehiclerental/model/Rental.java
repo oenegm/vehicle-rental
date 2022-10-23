6023add0765146ae57dc5355b526caf9ff4da253
M4Mohamed <mohammed.ramadan2000@gmail.com>
2022-10-23 14:42:51 +0200

    Add Rental, RentalStatus

---
+++ src/main/java/com/project/vehiclerental/model/Rental.java
@@ -0,0 +1,59 @@
+package com.project.vehiclerental.model;
+
+import com.project.vehiclerental.model.enums.RentalStatus;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "vehicle_ID",
+            referencedColumnName = "id",
+            nullable = false)
+    private Vehicle vehicle;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "user_ID",
+            referencedColumnName = "id",
+            nullable = false)
+    private User renter;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "owner_ID",
+            referencedColumnName = "id",
+            nullable = false)
+    private User owner;
+
+    @Column(name = "request_date", nullable = false)
+    private LocalDateTime requestDate;
+
+    @Column(name = "rent_date", nullable = false)
+    private LocalDateTime rentDate;
+
+    @Column(name = "return_date", nullable = false)
+    private LocalDateTime returnDate;
+
+    @Column(name = "rental_status", nullable = false)
+    private RentalStatus rentalStatus;
+
+    @Column(name = "duration", nullable = false)
+    private Double duration;
+
+    @Column(name = "rating", nullable = false)
+    private Double rating;
+
+    @Column(name = "cost", nullable = false)
+    private Double cost;
+
+
+}
---
+++ src/main/java/com/project/vehiclerental/model/enums/RentalStatus.java
@@ -0,0 +1,8 @@
+package com.project.vehiclerental.model.enums;
+
+public enum RentalStatus {
+    REQUESTED,
+    RENTED,
+    RETURNED,
+    CANCELLED
+}
