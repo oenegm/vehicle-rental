3fd91b324d98d2925ffbdf60557f21bd1e65186e
Mohammed Ramadan <mohammedramadan2000@gmail.com>
2022-12-12 10:01:31 +0200

    Add a simple username validity check
    Signed-off-by: Mohammed Ramadan <mohammedramadan2000@gmail.com>

--- src/main/java/com/project/vehiclerental/models/Rental.java
+++ src/main/java/com/project/vehiclerental/models/Rental.java
@@ -23,7 +23,7 @@ public class Rental {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(
             name = "vehicle_ID",
             referencedColumnName = "id",
