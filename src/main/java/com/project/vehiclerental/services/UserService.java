30f7e2a1ca91048598a28851c7e2149d03da1b7e
Mohammed Ramadan <mohammedramadan2000@gmail.com>
2023-01-27 13:17:32 +0200

    minor changes
    Signed-off-by: Mohammed Ramadan <mohammedramadan2000@gmail.com>

--- src/main/java/com/project/vehiclerental/services/UserService.java
+++ src/main/java/com/project/vehiclerental/services/UserService.java
@@ -3,6 +3,7 @@ package com.project.vehiclerental.services;
 import com.project.vehiclerental.exceptions.UserNotFoundException;
 import com.project.vehiclerental.models.User;
 import com.project.vehiclerental.repositories.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
