7350fa6f39c765619bfb001d5c58e516f4aad8bb
Mohammed Ramadan <mohammedramadan2000@gmail.com>
2023-08-09 00:11:15 +0300

    Implement FileService interface for Amazon S3 client
    This commit adds a new FileService interface and implements it for the Amazon S3 client. The S3 client is configured through the S3ClientConfig class, and the FileServiceController is implemented to handle file uploads to S3.
    
    The implementation includes a new uploadFile() method that takes a MultipartFile object and uploads it to S3. The uploaded file is given a unique filename and stored in the configured S3 bucket.
    
    This commit also includes unit tests for the FileService implementation, which ensure that files are uploaded correctly and that errors are handled properly.
    
    Signed-off-by: Mohammed Ramadan <mohammedramadan2000@gmail.com>

--- src/test/java/com/project/vehiclerental/WebLayerTest/UserControllerTest.java
+++ src/test/java/com/project/vehiclerental/WebLayerTest/UserControllerTest.java
@@ -1,4 +1,4 @@
-package com.project.vehiclerental.User;
+package com.project.vehiclerental.WebLayerTest;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.vehiclerental.controller.UserController;
 import com.project.vehiclerental.dto.UserDto;
+import com.project.vehiclerental.enums.Gender;
 import com.project.vehiclerental.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +19,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 
 import java.util.Arrays;
@@ -42,7 +42,7 @@ public class UserControllerTest {
         given(userService.getAllUser()).willReturn(userDtos);
         mockMvc.perform(get("/api/v1/users"))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().json(new ObjectMapper().writeValueAsString(userDtos)));
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(userDtos)));
     }
 
     @Test
@@ -52,31 +52,41 @@ public class UserControllerTest {
         given(userService.findUser(1L)).willReturn(userDto);
         mockMvc.perform(get("/api/v1/users/1"))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().json(new ObjectMapper().writeValueAsString(userDto)));
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(userDto)));
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = "IS_AUTHENTICATED_FULLY")
     public void testAddUser() throws Exception {
         UserDto userDto = new UserDto();
+
+        //set some values for the userDto
+        userDto.setId(1L);
+        userDto.setUsername("test");
+        userDto.setName("tester");
+        userDto.setGender(Gender.MALE);
+        userDto.setEmail("something@example.com");
+        userDto.setPassword("password");
+
         given(userService.saveUser(userDto)).willReturn(userDto);
         mockMvc.perform(post("/api/v1/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(userDto)))
                 .andExpect(status().isCreated())
-                .andExpect((ResultMatcher) content().json(new ObjectMapper().writeValueAsString(userDto)));
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(userDto)));
     }
 
     @Test
     @WithMockUser(roles = "ADMIN")
     public void testUpdateUser() throws Exception {
         UserDto userDto = new UserDto();
+
         given(userService.updateUser(1L, userDto)).willReturn(userDto);
         mockMvc.perform(put("/api/v1/users/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(userDto)))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().json(new ObjectMapper().writeValueAsString(userDto)));
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(userDto)));
     }
 
     @Test
@@ -88,7 +98,7 @@ public class UserControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(Collections.singletonMap("userDto", userDto))))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().json(new ObjectMapper().writeValueAsString(userDto)));
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(userDto)));
     }
 
     @Test
@@ -98,6 +108,6 @@ public class UserControllerTest {
         given(userService.deleteUser(1L)).willReturn(userDto);
         mockMvc.perform(delete("/api/v1/users/1"))
                 .andExpect(status().isNoContent())
-                .andExpect((ResultMatcher) content().json(new ObjectMapper().writeValueAsString(userDto)));
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(userDto)));
     }
 }
